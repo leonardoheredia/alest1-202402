c70de4a216d365cd8db1d928c50c9b0d755fe852
leonardoheredia <leonardo@heredia.com.br>
2024-08-07 16:19:58 -0300

    Aulas 01 e 02

---
+++ aula02_complexidade_algoritmos/ContagemOperacoes.java
@@ -0,0 +1,12 @@
+package aula02_complexidade_algoritmos;
+
+public class ContagemOperacoes {
+    public static void main(String[] args) {
+        int limite = 200;
+        int passo = 1;
+        System.out.println("n;funcao_01;funcao_02;funcao_03;funcao_04;funcao_05");
+        for (int n = 1; n <= limite; n+=passo) {
+     //       System.out.println(n + ";" + funcao_01(n) + ";" + funcao_02(n) + ";" + funcao_03(n) + ";" + funcao_04(n) + ";" + funcao_05(n));
+        }
+    }
+}
