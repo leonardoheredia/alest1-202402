61aded894abc55a40eeb9c11a3231d5797accc35
leonardoheredia <leonardo@heredia.com.br>
2024-08-19 18:49:34 -0300

    Aula 04 e 05

--- aula03_exercicios_funcao_complexidade/FuncoesComplexidade.java
+++ aula03_exercicios_funcao_complexidade/FuncoesComplexidade.java
@@ -3,9 +3,9 @@ package aula03_exercicios_funcao_complexidade;
 public class FuncoesComplexidade {
 
     public static void main(String[] args) {
-        System.out.println("n;alg_01");
-        for (int n = 1; n < 100; n++) {
-            System.out.println(n + ";" + alg_01(n));
+        System.out.println("n;alg_01;alg_02;alg_04;alg_05;alg_06");
+        for (int n = 10; n < 500; n+=10) {
+            System.out.println(n + ";" + alg_01(n) + ";" + alg_02(n) + ";" + alg_04(n) + ";" + alg_05(n) + ";" + alg_06(n));
         };
 
     }
@@ -18,6 +18,47 @@ public class FuncoesComplexidade {
         operacoes = 3*operacoes+3;
         return operacoes;
     }
+
+    public static int alg_02(int n) {
+        int operacoes = 0;
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                operacoes++;
+        return operacoes;
+    }
+
+    public static int alg_04(int n) {
+        int operacoes = 0;
+        for (int i = 0; i < n; i++)
+            for (int j = i; j < i + 2; j++)
+                for (int k = 0; k < n; k++)
+                    operacoes++;
+        return operacoes;
+    }
+
+    public static int alg_05(int n) {
+        int operacoes = 0;
+        for (int i = 0; i < n; i++)
+            for (int j = i; j < 2 * i; j++)
+                operacoes++;
+        return operacoes;
+    }
+
+    public static long alg_06(int n) {
+        long operacoes = 1;
+        for (int i = 1; i <= n / 2; i++) {
+            operacoes = operacoes * n;
+            if (operacoes >= Integer.MAX_VALUE) {
+                return Integer.MAX_VALUE;
+
+            }
+        }
+        return operacoes;
+    }
+
+
+
+
 
 
 }
