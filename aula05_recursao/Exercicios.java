401333e26f7f1072742ea4d73ac84e6e36308f45
leonardoheredia <leonardo@heredia.com.br>
2024-08-19 21:39:38 -0300

    Aula 05

--- aula05_recursao/Exercicios.java
+++ aula05_recursao/Exercicios.java
@@ -6,8 +6,30 @@ public class Exercicios {
         else return n + somatorio(n-1);
     }
 
+    public static int somatorioNaoRecursivo(int n) {
+        int soma = 0;
+        for (int i = n; i > 0; i--) {
+            soma = soma + i;
+        }
+        return soma;
+    }
+
+    public static int fatorialNaoRecursivo(int n) {
+        int fatorial = 1;
+        for (int i = n; i > 0; i--) {
+            fatorial = fatorial * i;
+        }
+        return fatorial;
+    }
+
     public static void main(String[] args) {
         int s = somatorio(4);
+        System.out.println(s);
+
+        s = somatorioNaoRecursivo(4);
+        System.out.println(s);
+
+        s = fatorialNaoRecursivo(5);
         System.out.println(s);
     }
 
