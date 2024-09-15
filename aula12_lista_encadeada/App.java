44c0f30e5a49209beba281202c3cd38482594b63
leonardoheredia <leonardo@heredia.com.br>
2024-09-15 20:11:44 -0300

    Aula 12 - Lista Linear Encadeada (Simples). Implementação completa

--- aula12_lista_encadeada/App.java
+++ aula12_lista_encadeada/App.java
@@ -2,14 +2,56 @@ package aula12_lista_encadeada;
 
 public class App {
     public static void main(String[] args) {
-        ListaSimplesmenteEncadeada lista = new ListaSimplesmenteEncadeada();
-        lista.adicionar("Guilherme");
-        lista.adicionar("Mauricio");
-        lista.adicionar("Amanda");
-        lista.adicionar("Joao");
-        System.out.println(lista);
+        ListaSimplesmenteEncadeada l = new ListaSimplesmenteEncadeada();
+        l.adicionar("leite");
+        l.adicionar("maca");
+        l.adicionar("pirulito");
+        System.out.println(l);
+        l.remover("maca");
+        System.out.println(l);
+        l.remover("leite");
+        System.out.println(l);
+        l.remover("pirulito");
+        System.out.println(l);
 
-        System.out.println(lista.existe("Amanda"));
-        System.out.println(lista.existe("Lucas"));
+        l.adicionar("mariana");
+        l.adicionar("leonardo");
+        l.adicionar("francisco");
+        System.out.println(l);
+        l.remover("francisco");
+        System.out.println(l);
+
+        l.adicionar("lucas");
+        System.out.println(l);
+
+        l.remover("mariana");
+        System.out.println(l);
+
+        l.remover("leonardo");
+        System.out.println(l);
+
+        l.remover("");
+        System.out.println(l);
+        l.remover("lucas");
+        System.out.println(l);
+
+        l.adicionar("mariana");
+        l.adicionar("leonardo");
+        l.adicionar("francisco");
+        System.out.println(l);
+        l.adicionar(0,"alexandre");
+        System.out.println(l);
+        l.adicionar(0,"tales");
+        System.out.println(l);
+        l.adicionar(3,"leandro");
+        System.out.println(l);
+        l.adicionar(6, "helena");
+        System.out.println(l);
+
+        l.limpar();
+        System.out.println(l);
+        l.adicionar("alexandre");
+        System.out.println(l);
+
     }
 }
--- aula12_lista_encadeada/ListaSimplesmenteEncadeada.java
+++ aula12_lista_encadeada/ListaSimplesmenteEncadeada.java
@@ -11,17 +11,18 @@ public class ListaSimplesmenteEncadeada {
     }
     private Nodo inicio;
     private Nodo fim;
-    private int quantidade;
+    private int tamanho;
 
     public ListaSimplesmenteEncadeada() {
         inicio = null;
         fim = null;
-        quantidade = 0;
+        tamanho = 0;
     }
 
     public void adicionar(String item) {
+        //por default adiciona no final da lista
         Nodo n = new Nodo(item);
-        if(quantidade==0) {
+        if(tamanho ==0) {
             inicio = n;
             fim = n;
         }
@@ -29,23 +30,93 @@ public class ListaSimplesmenteEncadeada {
             fim.proximo = n;
             fim = n;
         }
-        quantidade++;
+        tamanho++;
     }
 
     public boolean existe(String item) {
-        Nodo aux = inicio;
-        while (aux!=null) {
-            if(aux.item.equals(item)) return true;
-            aux = aux.proximo;
-        }
-        return false;
+        Nodo aux = buscarNodo(item);
+        return aux!=null;
     }
 
+    public int buscar(String item) {
+        int p = 0;
+        for(Nodo n = inicio; n!=null; n = n.proximo) {
+            if(n.item.equals(item)) return p;
+            p++;
+        }
+        return -1; //nao existe
+    }
 
+    public String buscar(int posicao) {
+        int p = 0;
+        for(Nodo n = inicio; n!=null; n = n.proximo) {
+            if(p == posicao) return n.item;
+        }
+        return null;
+    }
 
+    //public String buscar
+    private Nodo buscarNodo(String item) {
+        Nodo aux = inicio;
+        while(aux!=null) {
+            if(aux.item.equals(item)) return aux;
+        }
+        return null;
+    }
 
+    public void remover(String item) {
+        Nodo anterior = inicio;
+        for (Nodo r = inicio; r!=null; r = r.proximo) {
+            if(r.item.equals(item)) { //achou o nodo a remover
+                anterior.proximo = r.proximo;
+                if(r==inicio) inicio = r.proximo;
+                if(r==fim) fim = anterior;
+                tamanho--;
+                return;
+            }
+            anterior = r;
+        }
+    }
 
+    public boolean estaVazia() {
+        return tamanho==0;
+    }
 
+    public void adicionar(int posicao, String item) {
+        if (posicao < 0 || posicao > tamanho) {
+            throw new IndexOutOfBoundsException("Posicao invalida");
+        }
+        //adicionar em uma posicao especifica
+        Nodo n = new Nodo(item);
+
+        if(posicao==0) { //inserindo no inicio
+            n.proximo = inicio;
+            inicio = n;
+            if(tamanho==0) fim = n; //verifica tb o fim
+        }
+        else if(posicao==tamanho) {
+            //permite inserir no final
+            fim.proximo = n;
+            fim = n;
+        }
+        else {
+            Nodo anterior = inicio;
+            for(int p=1; p<posicao; p++) {
+                anterior = anterior.proximo;
+            }
+            //termina o laco com o anterior sendo o anterior do novo nodo
+            //inserir
+            n.proximo = anterior.proximo;
+            anterior.proximo = n;
+        }
+        tamanho++;
+    }
+    public void limpar() {
+        //limpa a lista
+        tamanho = 0;
+        inicio = null;
+        fim = null;
+    }
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder("{");
@@ -55,6 +126,10 @@ public class ListaSimplesmenteEncadeada {
             aux = aux.proximo;
         }
         sb.append("}");
+        if(!estaVazia()) {
+            sb.append(" inicio = ").append(inicio.item).append(" fim = ").append(fim.item);
+        }
+        sb.append(" tam = ").append(tamanho);
         return sb.toString();
     }
 }
