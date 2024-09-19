f0dc39415fe8124fe64f9d46118ceff3951d07b2
leonardoheredia <leonardo@heredia.com.br>
2024-09-18 21:53:17 -0300

    Aula 14 - Filas

--- aula14_filas/App.java
+++ aula14_filas/App.java
@@ -3,15 +3,12 @@ package aula14_filas;
 public class App {
     public static void main(String[] args) {
         FilaEstatica f = new FilaEstatica();
-        f.enfileirar("noteboook");
-        f.enfileirar("impressora");
-        f.enfileirar("mouse");
-        System.out.println(f.imprimirArray());
+        String s = f.desenfileirar();
+        System.out.println(s);
+        f.enfileirar("maria");
         System.out.println(f);
-        System.out.println(f.desenfileirar());
-        System.out.println(f.imprimirArray());
+        f.enfileirar("joao");
         System.out.println(f);
-        System.out.println(f.desenfileirar());
-        System.out.println(f);
+
     }
 }
--- aula14_filas/FilaEstatica.java
+++ aula14_filas/FilaEstatica.java
@@ -6,27 +6,38 @@ public class FilaEstatica {
     private String[] itens;
     private int inicio;
     private int fim;
+
     public FilaEstatica() {
-        capacidade = 4;
+        capacidade = 10;
         tamanho = 0;
         inicio = 0;
         fim = -1;
         itens = new String[capacidade];
     }
+
     public void enfileirar(String item) {
         //insere SEMPRE no final da fila
-        //final da lista sera fim+1;
+        //final da fila sera fim+1;
         int posicao = fim + 1;
         itens[posicao] = item;
         fim = posicao;
         tamanho++;
     }
+    public boolean estaVazia() {
+        return (tamanho==0);
+    }
+
     public String desenfileirar() {
+        if(estaVazia()) {
+            return null;
+        }
         String r = itens[inicio];
         itens[inicio] = null;
         inicio++;
+        tamanho--;
         return r;
     }
+
     public String imprimirArray() {
         StringBuilder sb = new StringBuilder("[ ");
         for (int i = 0; i < capacidade; i++) {
