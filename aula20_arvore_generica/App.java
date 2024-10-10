d53acbb8584cc39862a53fa9ff44b7f4aaf4ab2a
leonardoheredia <leonardo@heredia.com.br>
2024-10-09 21:57:35 -0300

    Aula 20 - Arvore Generica

--- aula20_arvore_generica/App.java
+++ aula20_arvore_generica/App.java
@@ -3,9 +3,9 @@ package aula20_arvore_generica;
 public class App {
     public static void main(String[] args) {
         Arvore t = new Arvore("R");
-        t.adicionarFilho("Z");
-        t.adicionarFilho("K");
-        t.adicionarFilho("P");
+        t.adicionarFilho("R", "Z");
+        t.adicionarFilho("R","K");
+        t.adicionarFilho("R", "P");;
         System.out.println(t);
     }
 }
--- aula20_arvore_generica/Arvore.java
+++ aula20_arvore_generica/Arvore.java
@@ -20,9 +20,23 @@ public class Arvore {
         Nodo n = new Nodo(rotulo);
         raiz = n;
     }
-    public void adicionarFilho(String rotulo) {
-        Nodo filho = new Nodo(rotulo);
-        raiz.adicionarFilho(filho);
+    public void adicionarFilho(String rotuloPai, String rotuloFilho) {
+        Nodo nodoFilho = new Nodo(rotuloFilho);
+        Nodo nodoPai = buscarNodo(rotuloPai);
+        nodoPai.adicionarFilho(nodoFilho);
+    }
+    private Nodo buscarNodo(String rotulo) {
+        return buscarNodoRecursivo(this.raiz, rotulo);
+    }
+    private Nodo buscarNodoRecursivo(Nodo n, String rotulo) {
+        if(n.rotulo.equals(rotulo)) return n;
+        else {
+            for (int i = 0; i < n.numFilhos; i++) {
+                Nodo nodoEncontrado = buscarNodoRecursivo(n.filhos[i], rotulo);
+                if(nodoEncontrado!=null) return nodoEncontrado;
+            }
+        }
+        return null;
     }
     @Override
     public String toString() {
