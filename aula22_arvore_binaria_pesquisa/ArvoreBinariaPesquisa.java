d3f8bba435ccaeaadf99abfecca7e040a4cd8feb
leonardoheredia <leonardo@heredia.com.br>
2024-10-21 21:35:18 -0300

    Aula 22 - Arvore Binaria

--- aula22_arvore_binaria_pesquisa/ArvoreBinariaPesquisa.java
+++ aula22_arvore_binaria_pesquisa/ArvoreBinariaPesquisa.java
@@ -1,6 +1,37 @@
 package aula22_arvore_binaria_pesquisa;
 
 public class ArvoreBinariaPesquisa {
+    private class Nodo {
+        private int chave;
+        private Nodo esquerda;
+        private Nodo direita;
+        public Nodo(int chave) {
+            this.chave = chave;
+        }
+    }
+    private Nodo raiz;
+
+    public ArvoreBinariaPesquisa() {};
+    public void adicionar(int chave) {
+        Nodo n = new Nodo(chave);
+        if(raiz==null) {
+            raiz = n;
+        }
+        else {
+            adicionarRecursivo(n, raiz);
+        }
+    }
+    private void adicionarRecursivo(Nodo n, Nodo pai) {
+        if(n.chave > pai.chave) { //descer para a DIREITA
+            if(pai.direita==null) pai.direita = n;
+            else adicionarRecursivo(n, pai.direita);
+        }
+        else {
+            if(pai.esquerda==null) pai.esquerda = n;
+            else adicionarRecursivo(n, pai.esquerda);
+        }
+    }
+
     /*
     public void imprimirArvore() {
         imprimirArvoreRecusivamente(this.raiz, 0);
